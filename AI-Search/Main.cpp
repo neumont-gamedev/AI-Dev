f7098cb560ef9f698d3965da7643a1d3774bea28
Raymond L Maple <rmaple@neumont.edu>
2025-02-01 12:03:27 -0700

    dfs - bfs

--- AI-Search/Main.cpp
+++ AI-Search/Main.cpp
@@ -2,12 +2,16 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <queue>
 
 using namespace std;
 using char_node_t = Node<char>;
 
 vector<char_node_t*> DFS(char_node_t* root, char target)
 {
+	// check for valid root node
+	if (root == nullptr) return vector<char_node_t*>();
+
 	stack<char_node_t*> node_stack;
 	root->visited = true;
 	node_stack.push(root);
@@ -55,6 +59,62 @@ vector<char_node_t*> DFS(char_node_t* root, char target)
 	return path;
 }
 
+vector<char_node_t*> BFS(char_node_t* root, char target)
+{
+	// check for valid root node
+	if (root == nullptr) return vector<char_node_t*>();
+
+	// create nodes queue and queue root onto stack
+	queue<char_node_t*> node_queue;
+	root->visited = true;
+	node_queue.push(root);
+
+	while (!node_queue.empty())
+	{
+		// get current node from the front of the queue
+		char_node_t* current = node_queue.front();
+
+		// check if the current node data is target value
+		if (current->data == target)
+		{
+			cout << "found: " << current->data << endl;
+			break;
+		}
+		else
+		{
+			cout << "visit: " << current->data << endl;
+		}
+
+		// if target not found then push all the children of the current node into the queue
+		for (auto child : current->children)
+		{
+			if (!child->visited)
+			{
+				child->visited = true;
+				child->parent = current;
+				node_queue.push(child);
+			}
+		}
+
+		// pop the front of the queue
+		node_queue.pop();
+	}
+
+	// convert nodes to vector of nodes (path)
+	// nodes are in reverse order with the parent used to move through the nodes
+	// add nodes to front of path vector
+	vector<char_node_t*> path;
+	char_node_t* node = (!node_queue.empty()) ? node_queue.front() : nullptr;
+	while (node)
+	{
+		// add node and then set node to node parent
+		path.insert(path.begin(), node);
+		node = node->parent;
+	}
+
+	return path;
+}
+
 int main()
 {
 	cout << "hello world!" << endl;
@@ -76,7 +136,7 @@ int main()
 	nodeC->children.push_back(nodeE);
 	nodeC->children.push_back(nodeF);
 
-	auto path = DFS(nodeA, 'F');
+	auto path = BFS(nodeA, 'F');
 
 	cout << "path: ";
 	for (auto node : path)
